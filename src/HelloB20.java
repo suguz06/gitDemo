eb7ebf9e242ec98d17392e06b2393fe4e9612d19
suguz06 <drselmanuguz@gmail.com>
2020-07-29 20:55:03 -0500

    first commit

---
+++ src/HelloB20.java
@@ -0,0 +1,8 @@
+public class HelloB20 {
+    public static void main(String[] args) {
+
+        System.out.println("HelloB20, it is my pleasure to be with you!");
+
+        //added a line
+    }
+}
